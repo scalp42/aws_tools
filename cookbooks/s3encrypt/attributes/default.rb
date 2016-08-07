8283f92818b3d2000c3258529650106ec32f1a1c
dtashner <davetashner@gmail.com>
2016-08-06 21:37:24 -0400

    genericizing default attributes

--- cookbooks/s3encrypt/attributes/default.rb
+++ cookbooks/s3encrypt/attributes/default.rb
@@ -1,5 +1,5 @@
 default['s3encrypt']['aws_region'] = 'us-east-1'
-default['s3encrypt']['encryption_context'] = "calvin_and_hobbes"
+default['s3encrypt']['encryption_context'] = nil
 default['s3encrypt']['local_secret_path'] = "#{::Chef::Config['file_cache_path']}/secrets.json"
-default['s3encrypt']['s3_secret_path'] = 'secrets/secrets.json'
-default['s3encrypt']['s3_bucket'] = 'dtashner'
+default['s3encrypt']['s3_secret_path'] = nil
+default['s3encrypt']['s3_bucket'] = nil
